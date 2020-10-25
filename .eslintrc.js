24549040a5cb109b116058e2e98703e28b209998
Andrey Simanovich <asksimon@yahoo.com>
2020-10-25 17:35:07 +0300

    WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  extends: ['airbnb-typescript', 'prettier'],
+  extends: ['react-app', 'airbnb-typescript', 'prettier'],
   parserOptions: {
     project: './tsconfig.json',
   },
